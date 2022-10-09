e3bd2c6a2c6ac621502a109a13f5d56b0d584b4b
fatihcevik <fatihcewik@gmail.com>
2022-10-09 12:58:47 +0300

    Auth added

--- src/http-common.ts
+++ src/http-common.ts
@@ -4,6 +4,8 @@ const apiClient= axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
+    "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNDI5MGVhZDVhNjlkNmZhMjA4OWM3YiIsImlhdCI6MTY2NTMwNzY2MSwiZXhwIjoxNjY1Mzk0MDYxfQ.SRt-WhhrRZcRBpqVak-gI_f7JrCU6wTpKWIvw-WYxjQ",
+    "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNDI5MGVhZDVhNjlkNmZhMjA4OWM3YiIsImlhdCI6MTY2NTMwNzY2MSwiZXhwIjoxNjY1Mzk0MDYxfQ.SRt-WhhrRZcRBpqVak-gI_f7JrCU6wTpKWIvw-WYxjQ" 
   },
 });
 
